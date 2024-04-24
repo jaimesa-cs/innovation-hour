e252ff885e553278c6dc4643d6d761934f33c38d
Jaime Santos Alcon <jaime.santosalcon@contentstack.com>
2024-04-23 22:12:12 -0400

    fixed href

--- app/[lng]/page/[...slug]/page.tsx
+++ app/[lng]/page/[...slug]/page.tsx
@@ -68,9 +68,9 @@ async function Home({ params }: any) {
           </a>
         </div>
       </div>
-      <div className="grid grid-cols-2 place-items-center ">
+      <div className="grid grid-rows-2 place-items-center ">
         <div className="text-s uppercase">{page?.title || "SELECT A PAGE ABOVE"}</div>
-        {page?.asset && assetUrl!== DEFAULT_IMAGE_URL && <div className="text-m">(<a href={`${assetUrl}`}>{assetUrl}</a>)</div>}
+        {page?.asset && <div className="text-m">(<a href={`${assetUrl.startsWith("http") ? assetUrl :`http://localhost:3000${assetUrl}`}`}>{assetUrl}</a>)</div>}
       </div>
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         
